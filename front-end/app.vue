e9cce462c011cc9a22cf29779578cae4fa952fae
Merlijn van den Berg <bergvandenmerlijn@gmail.com>
2025-02-28 17:19:24 +0100

    feat: updated readme and instructions

--- front-end/app.vue
+++ front-end/app.vue
@@ -4,18 +4,44 @@
 </script>
 
 <template>
-  <div class="flex flex-col items-center justify-center h-screen">
-    <h1 class="text-2xl font-xl text-slate-800">
-      BookThat Interview Assignment
-    </h1>
-    <p class="text-sm">Note: try to timebox 2h.</p>
-    <ul class="mt-10">
-      <li>1. As a user I want to use a from to create a todo.</li>
-      A todo has the following properties: - name - status: open, in progress,
-      done - due date
-      <li>2. As a user I want to see a list with all the todo's</li>
-      <li>3. As a user I want to be able to sort this list.</li>
-      <li>4. Optional: go funky on the styling.</li>
-    </ul>
+  <div
+    class="flex flex-col items-center justify-center min-h-screen bg-gray-50 px-6"
+  >
+    <div class="bg-white shadow-lg rounded-lg p-8 w-full max-w-lg">
+      <h1 class="text-3xl font-bold text-gray-800 text-center">
+        BookThat Interview Assignment
+      </h1>
+      <p class="text-sm text-gray-600 text-center mt-2">
+        Note: try to timebox 2h.
+      </p>
+
+      <ul class="mt-6 space-y-4 text-gray-700">
+        <li class="bg-gray-100 p-4 rounded-lg shadow-sm">
+          <span class="font-semibold">1.</span> As a user, I want to use a form
+          to create a todo.
+          <ul class="ml-4 mt-2 list-disc text-sm text-gray-600">
+            <li>A todo has the following properties:</li>
+            <li>- name</li>
+            <li>- status: open, in progress, done</li>
+            <li>- due date</li>
+          </ul>
+        </li>
+
+        <li class="bg-gray-100 p-4 rounded-lg shadow-sm">
+          <span class="font-semibold">2.</span> As a user, I want to see a list
+          with all the todos.
+        </li>
+
+        <li class="bg-gray-100 p-4 rounded-lg shadow-sm">
+          <span class="font-semibold">3.</span> As a user, I want to be able to
+          sort this list.
+        </li>
+
+        <li class="bg-gray-100 p-4 rounded-lg shadow-sm">
+          <span class="font-semibold">4.</span> Optional: Go funky on the
+          styling.
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
--- readme.md
+++ readme.md
@@ -1,3 +1,23 @@
-1. in `/strapi` and `/front-end` run `npm install`.
-2. in `/strapi` and `/front-end` run `npm dev`.
-3. Check `app.vue` for instructions.
+# BookThat Interview Assignment
+
+## Setup Instructions
+
+### 1. Install Dependencies
+
+Run the following command in both the `/strapi` and `/front-end` directories:
+
+```sh
+npm install
+```
+
+### 2. Start the Development Servers
+
+In both `/strapi` and `/front-end` directories, start the servers by running:
+
+```sh
+npm run dev
+```
+
+### 3. Follow the Instructions
+
+Check the `app.vue` file in the `/front-end` directory for further instructions on completing the exercise.
